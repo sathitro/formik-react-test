d7ecd64d0be007a74da601337023c09317a248f5
ikeng <satidasss@gmail.com>
2020-09-10 15:41:26 +0700

    original

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// reference
+// https://medium.com/the-existing/validation-register-form-within-15-minutes-bc65e827c128
+
+import React from 'react'
+import RegisterForm from './Register/RegisterForm'
+
+function App() {
+  return (
+    <div>
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
